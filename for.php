ca14c1ed79b30850d576cb00175e05a5acb31a1c
Riko Syam Ramadhan <60320482+rikosss@users.noreply.github.com>
2021-03-18 11:39:12 +0700

    Add files via upload

---
+++ for.php
@@ -0,0 +1,22 @@
+ <?php  
+      for ($i=1; $i<=5;$i++)
+      {  
+           for($j=1; $j<=$i;$j++)
+           {  
+                // echo $i;
+                if ($i==3 AND $j==2 ) {
+                	echo "x";
+                	}else{
+                		echo "$i";
+                	}
+                 
+                 	
+              }
+
+				                        
+
+               echo '<br/>';     	
+                        
+          }
+        
+ ?>  
